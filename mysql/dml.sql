8da1718c66163ebcf5022dd5db5a242bbaffcf41
himajayadavgellu <149390033+himajayadavgellu@users.noreply.github.com>
2025-10-21 18:25:05 +0530

    regex in java,SQL commands on 21 oct 2025

---
+++ mysql/dml.sql
@@ -0,0 +1,8 @@
+//Data Manipulation Language(insert).Inserting values into customer table.
+
+INSERT INTO customer (customernumber, lastname, firstname, address)
+VALUES 
+	(101, 'Yadav', 'Himaja', '123 Street, Kuala Lumpur'),
+	(102, 'Tan', 'Wei', '456 Jalan Bukit, Kuala Lumpur'),
+	(103, 'Smith', 'John', '789 Avenue, Kuala Lumpur'),
+	(104, 'Lee', 'Siti', '101 Jalan Ampang, Kuala Lumpur');
